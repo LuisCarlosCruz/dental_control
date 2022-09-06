a83113203e27f295f7506e9d3c73b0db8a2c3108
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-09-06 10:55:03 -0300

    include input filter e onchanges

--- src/pages/filter/Filter.jsx
+++ src/pages/filter/Filter.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import Input from '../../components/input/Input';
+import convertDate from '../../utils/convertDate';
 
 const Filter = () => {
+  const handleInitial = (date) => {
+    const newDate = convertDate(date);
+    console.log(newDate);
+  };
+
+  const handleFinal = (date) => {
+    const newDate = convertDate(date);
+    console.log(newDate);
+  };
+
   return (
     <div>
-      <h2>FILTRO</h2>
-      <input placeholder="data inicial"></input>
-      <input placeholder="data final"></input>
+      <h2>FILTRAR POR PERIODO</h2>
+      <label>
+        data inicial:
+        <Input type="date" onChange={(date) => handleInitial(date)} />
+      </label>
+      <label>
+        data final:
+        <Input type="date" onChange={(date) => handleFinal(date)} />
+      </label>
+      <button>voltar</button>
       <button>Filtrar</button>
     </div>
   );
