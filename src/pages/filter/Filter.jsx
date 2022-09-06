9f9995f1d8554a455acfb598d6a300ce43dc000c
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-09-06 09:10:12 -0300

    include page filter

---
+++ src/pages/filter/Filter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Filter = () => {
+  return (
+    <div>
+      <h2>FILTRO</h2>
+      <input placeholder="data inicial"></input>
+      <input placeholder="data final"></input>
+      <button>Filtrar</button>
+    </div>
+  );
+};
+
+export default Filter;
