f9905c93efe8490054c50ef196041c3d56578f76
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-09-06 22:42:20 -0300

    include handleFilter initial

--- src/pages/filter/Filter.jsx
+++ src/pages/filter/Filter.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Input from '../../components/input/Input';
 import convertDate from '../../utils/convertDate';
+import MainContext from '../../store/context/index';
+import Button from '../../components/button/Button';
+import periodFilter from '../../utils/periodFilter';
 
 const Filter = () => {
+  const {
+    initialPeriod,
+    setInitialPeriod,
+    finalPeriod,
+    setFinalPeriod,
+    allPatients,
+    filteredPatients,
+    setFilteredPatients
+  } = useContext(MainContext);
+
   const handleInitial = (date) => {
     const newDate = convertDate(date);
-    console.log(newDate);
+    setInitialPeriod(newDate);
   };
 
   const handleFinal = (date) => {
     const newDate = convertDate(date);
-    console.log(newDate);
+    setFinalPeriod(newDate);
+  };
+
+  const handleFilter = () => {
+    // VALIDAR AS DATAS ANTES DE FILTRAR
+    const patientsFilter = periodFilter(initialPeriod, finalPeriod, allPatients);
+    setFilteredPatients(patientsFilter);
   };
 
   return (
@@ -24,8 +43,28 @@ const Filter = () => {
         data final:
         <Input type="date" onChange={(date) => handleFinal(date)} />
       </label>
-      <button>voltar</button>
-      <button>Filtrar</button>
+      <Button onClick={() => history.back()} text="Voltar" />
+      <Button onClick={() => handleFilter()} text="Filtrar" />
+      <Button onClick={() => {}} text="Limpar" />
+      <table>
+        <tbody>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>valorParcela</th>
+            <th>DataPagamento</th>
+          </tr>
+          {filteredPatients.length > 0 &&
+            filteredPatients.map((patient) => (
+              <tr key={patient.id}>
+                <td>{patient.id}</td>
+                <td>{patient.name}</td>
+                <td>{patient.parcelValue}</td>
+                <td>{patient.date}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 };
