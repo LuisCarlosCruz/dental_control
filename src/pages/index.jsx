c49eb21fd804b0d66a7e2bb4bc498bb20d0fd2df
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-09-08 23:09:52 -0300

    include procedures in pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
 import Home from './home';
 import Filter from './filter/Filter';
 import Payments from './payments/Payments';
+import Procedures from './procedures/Procedures';
 
 const PAGES = {
   Home,
   Filter,
-  Payments
+  Payments,
+  Procedures
 };
 
 export default PAGES;
