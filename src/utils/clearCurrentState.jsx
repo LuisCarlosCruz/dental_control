a73cc81d95679523126b773b6e1c06fe78448eb5
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-09-12 11:45:08 -0300

    update clear current state

--- src/utils/clearCurrentState.jsx
+++ src/utils/clearCurrentState.jsx
@@ -1,5 +1,12 @@
-const clearCurrentState = (setName, setValueProcedure, setNumberParcels, setDateStartPayment) => {
+const clearCurrentState = (
+  setName,
+  setProcedure,
+  setValueProcedure,
+  setNumberParcels,
+  setDateStartPayment
+) => {
   setName('');
+  setProcedure([]);
   setValueProcedure(0);
   setNumberParcels(0);
   setDateStartPayment('');
