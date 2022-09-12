9f76ab980a8680371e9e0e13bb0350eda74e7523
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-09-12 11:43:53 -0300

    include toast notify

---
+++ src/utils/toastNotify.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const notifySuccess = (message) =>
+  toast.success(message, { position: 'top-center', autoClose: 2000 });
+
+const notifyWarning = (message) =>
+  toast.warning(message, { position: 'top-center', autoClose: 2000 });
+
+const notifyError = (message) => toast.error(message, { position: 'top-center', autoClose: 2000 });
+
+const ToastNotify = () => {
+  return (
+    <div>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export { ToastNotify, notifySuccess, notifyWarning, notifyError };
