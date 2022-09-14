8f7a51626a4b697f24fa87fe961bacb430cec818
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-09-13 22:11:03 -0300

    update component input

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 const Input = ({ type, placeholder, onChange }) => {
   return (
-    <div>
-      <label>
-        <input
-          required
-          type={type}
-          placeholder={placeholder}
-          onChange={({ target }) => onChange(target.value)}></input>
-      </label>
-    </div>
+    <>
+      <input
+        className="input"
+        required
+        type={type}
+        placeholder={placeholder}
+        onChange={({ target }) => onChange(target.value)}></input>
+    </>
   );
 };
 
