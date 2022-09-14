a4473f972aa663dda46c37433d9f3f44111ba958
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-09-13 22:11:23 -0300

    update component select

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -4,16 +4,23 @@ import MainContext from '../../store/context';
 const Select = ({ onChange }) => {
   const { procedureList } = useContext(MainContext);
   return (
-    <select name="procedures" required defaultValue="" onChange={(e) => onChange(e)}>
-      <option value="" disabled hidden>
-        escolher procedimento
-      </option>
-      {procedureList.map((procedure, index) => (
-        <option key={index} value={procedure}>
-          {procedure}
+    <>
+      <select
+        className="input"
+        name="procedures"
+        required
+        defaultValue=""
+        onChange={(e) => onChange(e)}>
+        <option value="" disabled hidden>
+          escolher procedimento
         </option>
-      ))}
-    </select>
+        {procedureList.map((procedure, index) => (
+          <option key={index} value={procedure}>
+            {procedure}
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
 
